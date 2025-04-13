09791f7a7c49e9f2a6dd46885de12b17fae7b9e0
absus_103927 <absuslo@gmail.com>
2025-04-13 11:45:03 +0200

    Zmiana klasy PatientMapper na final

--- wsb-jpa1/src/main/java/com/jpacourse/mapper/PatientMapper.java
+++ wsb-jpa1/src/main/java/com/jpacourse/mapper/PatientMapper.java
@@ -3,7 +3,7 @@ package com.jpacourse.mapper;
 import com.jpacourse.dto.PatientTO;
 import com.jpacourse.persistance.entity.PatientEntity;
 
-public class PatientMapper {
+public final class PatientMapper {
     public static PatientTO mapToTO(final PatientEntity patientEntity) {
         if(patientEntity == null) {
             return null;
