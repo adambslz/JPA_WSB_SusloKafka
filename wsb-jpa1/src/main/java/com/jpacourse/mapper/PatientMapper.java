0f2dad03165724643360077a360261f02e337b17
absus_103927 <absuslo@gmail.com>
2025-04-13 11:43:16 +0200

    Utworzony podstawowy Mapper i TO

---
+++ wsb-jpa1/src/main/java/com/jpacourse/mapper/PatientMapper.java
@@ -0,0 +1,29 @@
+package com.jpacourse.mapper;
+
+import com.jpacourse.dto.PatientTO;
+import com.jpacourse.persistance.entity.PatientEntity;
+
+public class PatientMapper {
+    public static PatientTO mapToTO(final PatientEntity patientEntity) {
+        if(patientEntity == null) {
+            return null;
+        };
+        final PatientTO patientTO = new PatientTO();
+        patientTO.setId(patientEntity.getId());
+        patientTO.setFirstName(patientEntity.getFirstName());
+        patientTO.setLastName(patientEntity.getLastName());
+        patientTO.setEmail(patientEntity.getEmail());
+        return patientTO;
+    }
+    public static PatientEntity mapToEntity(final PatientTO patientTO) {
+        if(patientTO == null) {
+            return null;
+        }
+        final PatientEntity patientEntity = new PatientEntity();
+        patientEntity.setId(patientTO.getId());
+        patientEntity.setFirstName(patientTO.getFirstName());
+        patientEntity.setLastName(patientTO.getLastName());
+        patientEntity.setEmail(patientTO.getEmail());
+        return patientEntity;
+    }
+}
