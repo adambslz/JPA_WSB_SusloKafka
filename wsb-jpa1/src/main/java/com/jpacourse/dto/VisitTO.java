517387776e22911109bb56e239bd8b42432e6acc
ARTUA <spamr08@gmail.com>
2025-04-14 22:02:54 +0200

    Utworzono VisitTO oraz DoctorTO

---
+++ wsb-jpa1/src/main/java/com/jpacourse/dto/VisitTO.java
@@ -0,0 +1,62 @@
+package com.jpacourse.dto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class VisitTO implements Serializable {
+    private Long id;
+    private String description;
+    private LocalDateTime time;
+    private PatientTO patient;
+    private DoctorTO doctor;
+    private List<MedicalTreatmentTO> medicalTreatments;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public PatientTO getPatient() {
+        return patient;
+    }
+
+    public void setPatient(PatientTO patient) {
+        this.patient = patient;
+    }
+
+    public DoctorTO getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(DoctorTO doctor) {
+        this.doctor = doctor;
+    }
+
+    public List<MedicalTreatmentTO> getMedicalTreatments() {
+        return medicalTreatments;
+    }
+
+    public void setMedicalTreatments(List<MedicalTreatmentTO> medicalTreatments) {
+        this.medicalTreatments = medicalTreatments;
+    }
+}
