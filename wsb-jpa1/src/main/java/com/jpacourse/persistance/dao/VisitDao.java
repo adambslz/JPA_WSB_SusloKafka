98ee343a8e3768bb128b080d8cbe66d79a49065c
absus_103927 <absuslo@gmail.com>
2025-05-13 00:00:47 +0200

    Revert "Utworzono VisitTO oraz DoctorTO"
    This reverts commit ddb3a3fd

--- wsb-jpa1/src/main/java/com/jpacourse/persistance/dao/VisitDao.java
+++
@@ -1,8 +0,0 @@
-package com.jpacourse.persistance.dao;
-
-import com.jpacourse.persistance.entity.VisitEntity;
-import java.util.List;
-
-public interface VisitDao extends Dao<VisitEntity, Long>{
-    List<VisitEntity> findAllByPatientId(Long patientId);
-}
